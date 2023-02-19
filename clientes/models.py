65efb54532a4f0941682b5f893405274e1bd61a4
SeverinoRibNeto <severino.neto@dcx.ufpb.br>
2023-02-19 14:55:23 -0300

    Classe Pessoa criada;

--- clientes/models.py
+++ clientes/models.py
@@ -1,1 +1,13 @@
 from django.db import models
+
+
+class Pessoa(models.Model):
+    nome = models.CharField(max_length=255)
+    sobrenome = models.CharField(max_length=255)
+    data_aniversario = models.DateField()
+    telefone = models.CharField(max_length=13)
+    email = models.EmailField(max_length=100)
+    endereco = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nome
