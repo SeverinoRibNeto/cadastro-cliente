3511bef17954922b1cadbb84e39977e58b9cbdd9
SeverinoRibNeto <severino.neto@dcx.ufpb.br>
2023-02-19 14:55:53 -0300

    Adicionada paths de urls

--- clientes/urls.py
+++ clientes/urls.py
@@ -2,6 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.cadastrar_cliente, name='cadastrar_cliente'),
-    path('buscar/', views.buscar_cliente, name='buscar_cliente')
+    path('', views.home_url, name='home_url'),
+    path('cadastro/', views.cadastrar_cliente, name='cadastrar_cliente'),
+    path('salvaCliente/', views.salva_cliente, name='salva_cliente'),
+    path('alterarCliente/<int:id>', views.altera_cliente, name='altera_cliente'),
+    path('atualiza_cliente/<int:id>',
+         views.atualiza_cliente, name='atualiza_cliente'),
+    path('buscar/', views.buscar_cliente, name='buscar_cliente'),
+    path('filtraCliente/',
+         views.filtra_cliente, name='filtra_cliente')
 ]
